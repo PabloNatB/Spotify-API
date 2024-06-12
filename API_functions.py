bfc6c4b11a3cffc2fba0b1b85245dcf880a8737a
PabloNatB <pablonatera16@gmail.com>
2024-06-12 01:25:43 -0600

    Second commit

--- API_functions.py
+++ API_functions.py
@@ -11,91 +11,156 @@ import json
 import pandas as pd
 from skimage import io
 import matplotlib.pyplot as plt
+import sqlite3
+
+class spotify_top_ten_songs:
+    
+    __slots__ = ["__client_ID" , "__client_secret" , "__token" , "__df" , "__artist_name" ]
+    
+    @staticmethod
+    def get_token(self):
+        # Encode client credentials
+        auth_string = f"{self.__client_ID}:{self.__client_secret}"
+        auth_bytes = auth_string.encode("utf-8")
+        auth_base64 = base64.b64encode(auth_bytes).decode("utf-8")
+        
+        url = "https://accounts.spotify.com/api/token"
+        headers = {
+            "Authorization": f"Basic {auth_base64}",
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+        
+        data = {"grant_type": "client_credentials"}
+        
+        result = post(url, data=data, headers=headers)
+        
+        if result.status_code != 200:
+            print("Error:", result.json())
+            return None
+        
+        json_result = result.json()
+        token = json_result.get("access_token")
+        return token
+    
+    def __init__(self,client_id,client_secret):
+        self.__client_ID = client_id
+        self.__client_secret = client_secret
+        self.__token = self.get_token(self)
+        
+    @property
+    def artist_name(self):
+        return self.__artist_name
+    
+    @artist_name.setter
+    def artist_name(self,value):
+        if not isinstance(value, str):
+            raise TypeError("Artist name must be a string")
+        self.__artist_name = value
 
 
-def get_token(client_ID,client_secret):
-    # Encode client credentials
-    auth_string = f"{client_ID}:{client_secret}"
-    auth_bytes = auth_string.encode("utf-8")
-    auth_base64 = base64.b64encode(auth_bytes).decode("utf-8")
+    @staticmethod
+    def get_auth_header(obj):
+        return {"Authorization": "Bearer " + obj.__token}
     
-    url = "https://accounts.spotify.com/api/token"
-    headers = {
-        "Authorization": f"Basic {auth_base64}",
-        "Content-Type": "application/x-www-form-urlencoded"
-    }
+    @staticmethod
+    def search_artist( obj  ):
+        url = "https://api.spotify.com/v1/search"
+        headers = obj.get_auth_header(obj)
+        query = f"?q={obj.__artist_name}&type=artist&limit=1"
+        
+        query_complete = url + query
+        
+        results = get(query_complete,headers=headers)
+        json_res=json.loads(results.content)["artists"]["items"]
+        return json_res[0]
     
-    data = {"grant_type": "client_credentials"}
+    @staticmethod
+    def get_songs_artist( obj , artist_id ):
+        url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=MX"
+        headers = obj.get_auth_header(obj)
+        res = get(url,headers=headers)
+        json_res = json.loads(res.content)["tracks"]
+        return json_res
     
-    result = post(url, data=data, headers=headers)
     
-    if result.status_code != 200:
-        print("Error:", result.json())
-        return None
+    def search(self):
+        result=self.search_artist(self )
+        
+        artist_id=result["id"]
+        
+        songs=self.get_songs_artist(self , artist_id )
+        
+        self.__df=pd.DataFrame(columns=["Place","Song Name", "Album","Duration in minutes","Image URL"] )
+        
+        for i,song in enumerate(songs):
+            #print(i+1,"._ ",song["name"],", ", song["album"]["name"] ,", ", round(song['duration_ms']/(60_000),2),sep="")
+            self.__df.loc[i]=[i + 1 , song["name"] , song["album"]["name"] , round(song['duration_ms']/(60_000),2 ), song["album"]["images"][0]["url"]]
+        
+        urls  = list( self.__df["Image URL"] )
+        names = list( self.__df["Song Name"] )
+        
+        #df_name= "Top 10 " + self.__artist_name + " songs.csv"
+        #self.__df.set_index('Place', inplace = True)
+        #self.__df.to_csv(df_name)
+        
+        fig , axs = plt.subplots(3,3)
+        for i in range(3):
+            for j in range(3):
+                index=j + 3*i
+                image = io.imread(urls[index])
+                axs[i,j].imshow(image)
+                axs[i,j].axis("off")
+                title= str(index + 1) + ". " + names[index]
+                axs[i,j].set_title(title,size=7)
+                
+        title="Top 9 songs from: " + self.__artist_name
+        fig.suptitle(title)    
+        # Set background color for the entire figure
+        fig.patch.set_facecolor('#556B2F')
+        
+        plot_name = "Top 9 " + self.__artist_name + " songs.png" 
+        fig.savefig(plot_name, dpi=300)
+        
+    @staticmethod
+    def df_to_tuple(df):
+        for i in range(len(df)):
+            yield tuple( df.loc[ i ]) 
+             
+        
+        
+        
+        
+    def to_database(self):
+        
+        database_name= self.__artist_name.replace(" ","_") + ".db"
+        print(database_name)
+        conn = sqlite3.connect(database_name)
+        cursor = conn.cursor()
+        
+        #preparing statement
+        sql1 = f"DROP TABLE IF EXISTS {database_name}" 
+        sql2 = f'''CREATE TABLE {database_name} (
+        
+        Place INT NOT NULL,
+        Song name CHAR(100) NOT NULL,
+        Album CHAR(100),
+        Duration FLOAT,
+        Image_URL CHAR(255)
+        
+        )'''
+        cursor.execute(sql1)
+        cursor.execute(sql2)
+        
+        sql3 = "INSERT INTO " + database_name + " VALUES (?,?,?,?,?)"
+        data = self.df_to_tuple(self.__df)
+        for _ in range( len( self.__df ) ):
+            cursor.execute(sql3,next(data))
+        conn.close()
     
-    json_result = result.json()
-    token = json_result.get("access_token")
-    return token
-
-
-
-def get_auth_header(token):
-    return {"Authorization": "Bearer " + token}
-
-
-def search_artist(token,name):
-    url = "https://api.spotify.com/v1/search"
-    headers = get_auth_header(token)
-    query = f"?q={name}&type=artist&limit=1"
-    
-    query_complete= url + query
-    
-    results = get(query_complete,headers=headers)
-    json_res=json.loads(results.content)["artists"]["items"]
-    return json_res[0]
-
-
-def get_songs_artist(token,artist_id):
-    url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=MX"
-    headers = get_auth_header(token)
-    res = get(url,headers=headers)
-    json_res = json.loads(res.content)["tracks"]
-    return json_res
-
-
-def search(token,name):
-    result=search_artist(token, name)
-    
-    artist_id=result["id"]
-    
-    songs=get_songs_artist(token, artist_id)
-    
-    df=pd.DataFrame(columns=["Place","Song Name", "Album","Duration in minutes","Image URL"])
-    
-    for i,song in enumerate(songs):
-        #print(i+1,"._ ",song["name"],", ", song["album"]["name"] ,", ", round(song['duration_ms']/(60_000),2),sep="")
-        df.loc[i]=[i + 1 , song["name"] , song["album"]["name"] , round(song['duration_ms']/(60_000),2 ), song["album"]["images"][0]["url"]]
-    
-    urls  = list( df["Image URL"] )
-    names = list( df["Song Name"] )
-    
-    df_name= "Top 10 " + name + " songs.csv"
-    df.to_csv(df_name)
-    
-    fig , axs = plt.subplots(3,3)
-    for i in range(3):
-        for j in range(3):
-            index=j + 3*i
-            image = io.imread(urls[index])
-            axs[i,j].imshow(image)
-            axs[i,j].axis("off")
-            title= str(index + 1) + ". " + names[index]
-            axs[i,j].set_title(title,size=7)
-            
-    title="Top 9 songs from: " + name
-    fig.suptitle(title)    
-    # Set background color for the entire figure
-    fig.patch.set_facecolor('#556B2F')
-    
-    plot_name = "Top 9 " + name + " songs.png" 
-    fig.savefig(plot_name, dpi=300)
+        
+        
+        
+        
+        
+        
+        
--- Spotify API.py
+++ Spotify API.py
@@ -6,7 +6,7 @@ Created on Mon Jun 10 18:22:45 2024
 """
 
 
-from API_functions import search,get_token
+from API_functions import spotify_top_ten_songs
 
 
 
@@ -17,9 +17,11 @@ file.close()
 client_ID=lines[0][:-1]
 client_secret=lines[1]
 
-token = get_token(client_ID,client_secret)
+top_ten = spotify_top_ten_songs(client_ID, client_secret)
        
 
 names=["Thom Yorke" , "Radiohead" , "Interpol" , "Arctic Monkeys" , "Muse" , "Deftones"]
 for name in names:
-    search(token,name)
+    top_ten.artist_name = name
+    top_ten.search()
+    top_ten.to_database()
